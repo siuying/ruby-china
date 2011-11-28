c85bba661b8aab2d86e607403d965a1a5e12e3af
Francis Chong <francis@ignition.hk>
2011-11-29 01:28:04 +0800

    publish message when a mention is created

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,7 @@
+require 'pusher'
+
 pusher_config = YAML.load_file("#{Rails.root}/config/pusher.yml")[Rails.env] rescue nil
 if pusher_config
-  require 'pusher'
   Pusher.app_id = pusher_config["app_id"]
   Pusher.key    = pusher_config["key"]
   Pusher.secret = pusher_config["secret"]
